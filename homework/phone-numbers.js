3f27ff7512587fc0bd5b4f5fb77736ecd2b735ae
dtr200 <negevski@yandex.ru>
2021-06-08 00:06:17 +0300

    the task solution has been fixed

--- homework/phone-numbers.js
+++ homework/phone-numbers.js
@@ -2,8 +2,9 @@ function isNumberExist(values){
     const nums = values.map(num => {
         const purified = num.replace(/[^0-9]/g, '');
         if(purified.length === 11)
-            return purified.substring(4);
-        return purified
+            return purified.substring(1);
+        if(purified.length === 7)
+            return `495${purified}`;
     })
 
     const wannaAdd = nums.shift();
@@ -11,7 +12,7 @@ function isNumberExist(values){
     const result = nums.map(num => 
         num === wannaAdd ? 'YES' : 'NO')
 
-    return result.join(' ');
+    return result.join(' '));
 }
 
 
@@ -20,18 +21,4 @@ isNumberExist([
     '+7-4-9-5-43-023-97', 
     '4-3-0-2-3-9-7', 
     '8-495-430'
-]);
-
-isNumberExist([
-    '86406361642',
-    '83341994118',
-    '86406361642',
-    '83341994118'
-]);
-
-isNumberExist([
-    '+78047952807',
-    '+78047952807',
-    '+76147514928',
-    '88047952807'
 ]);
