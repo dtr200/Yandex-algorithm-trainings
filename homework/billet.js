eba81cbf4d8aac607d085321a87b4fc5e7c85afb
dtr200 <negevski@yandex.ru>
2021-06-12 15:53:09 +0300

    billet task almost completed

--- homework/billet.js
+++ homework/billet.js
@@ -1,12 +1,25 @@
-function getBillets(str){
-    const [
+function calcBillets(str){
+    if(!str) return 0;
+    const val = str.split(' ').map(num => Number(num));
+    let [
         alloy,
         billet,
         detail
-    ] = str.split(' ')
-           .map(num => Number(num));
+    ] = val;
 
-    
+    if((!alloy || !billet || !detail) ||
+        (detail > billet))
+        return 0;
+
+    let details = 0;
+
+    while(alloy >= billet && billet >= detail){
+        details += Math.floor(billet/detail);
+        alloy -= billet;
+        alloy += billet % detail;
+    }
+
+    return `${details}`;
 }
 
-getBillets('10 5 2')
+calcBillets('13 5 3');
---
+++ homework/emergency.js
@@ -0,0 +1,12 @@
+function findAddress(values){
+    const [ k1, m, k2, p2, n2 ] = values;
+
+    const p1 = Math.ceil(k1/(k2/n2*m));
+
+    const n1 = Math.ceil
+    console.log(p1, n1)
+}
+
+findAddress([169, 20, 41, 1, 11]);
+//findAddress([11, 1, 1, 1, 1]);
+/* findAddress([19, 1, 11, 2, 1]);*/
