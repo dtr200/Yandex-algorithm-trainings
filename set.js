760d2a11e4b44ddaf306fdc2667243fc537fc481
dtr200 <negevski@yandex.ru>
2021-06-30 23:48:02 +0300

    Lecture-3: task 2 added

--- set.js
+++ set.js
@@ -38,3 +38,48 @@ function effectivegetMaxTwo(nums, x){
 }
 
 effectivegetMaxTwo([1,2,4,7,2], 11);
+
+/*
+    Дан словарь из N слов, длина каждого не превосходит К.
+    В записи каждого из М слов текста (каждое длиной до К)
+    может быть пропущена одна буква. Для каждого слова
+    сказать, входит ли оно (возможно, с одной пропущенной
+    буквой) в словарь
+*/
+
+/* Решение: Выбрасываю из каждого слова по одной букве 
+всеми способами за O(n * k) и ложу эти слова в сет. Дальше
+проверяю есть ли такое слово за О(1). 
+Сложность: O(NK**2 + M), где
+N - количество возможных слов в словаре,
+К - перебор позиции внутри слова, где буду удалять по букве +
+    конструирование нового слова без буквы К - 1,
+М - слова из text (2-й параметр) */
+
+function checkWord(dictionary, text){
+    const goodWords = new Set(dictionary);
+
+    for(let word of dictionary){
+        const currentWord = word.split('');
+        for(let i = 0; i < currentWord.length; i++){
+            const newWord = [
+                ...currentWord.slice(0, i),
+                ...currentWord.slice(i + 1)
+            ]
+            goodWords.add(newWord.join(''));
+        }
+    }
+    const answer = [];
+    for(let word of text){
+        answer.push(goodWords.has(word))
+    }
+    return answer;
+}
+
+/* Это слово даст варианты: abcd, bcd, acd, abd, abc. Если в 
+слове К букв, количество возможных слов = К + 1 */
+
+checkWord(
+    ['abcd', 'jfgt', 'opet'],
+    ['bcd', 'fgt', 'rrt']
+);
